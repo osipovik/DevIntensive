a73ce197f85d20f9889f78ea9d6210e6cdb2ca19
Ivan Osipov <osipovik89@gmail.com>
2016-06-20 23:13:05 +0300

    add dimens, fix colors name, change .gitignore

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -11,11 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
     }
 }
