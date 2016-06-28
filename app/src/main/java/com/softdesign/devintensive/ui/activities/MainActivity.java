b4dfd3df4b4741ca7d5bf043c8eb63eee578f1fd
Ivan Osipov <osipovik89@gmail.com>
2016-06-29 00:41:57 +0300

    Добавил FloatingActionButton, и смену режима view(просмотр/редактирование)

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,7 +1,9 @@
 package com.softdesign.devintensive.ui.activities;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
@@ -10,15 +12,28 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
 
-public class MainActivity extends BaseActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends BaseActivity implements View.OnClickListener {
     private final String TAG = ConstantManager.TAG_PREFIX  + getClass().getSimpleName();
+
     private CoordinatorLayout mCoordinatorLayout;
     private Toolbar mToolbar;
     private DrawerLayout mNavigationDrawer;
+    private FloatingActionButton mFloatingActionButton;
+    private EditText mUserPhone, mUserEmail, mUserVk, mUserGit, mUserAbout;
+
+    private List<EditText> mUserInfo;
+
+    private int mCurrentEditMode = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +43,34 @@ public class MainActivity extends BaseActivity {
         mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_coordinator_layout);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mNavigationDrawer = (DrawerLayout) findViewById(R.id.navigation_drawer);
+        mFloatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+
+        //TODO Использовать для инициализации ButterKnife
+        mUserPhone = (EditText) findViewById(R.id.phone_et);
+        mUserEmail = (EditText) findViewById(R.id.email_et);
+        mUserVk = (EditText) findViewById(R.id.vk_id_et);
+        mUserGit = (EditText) findViewById(R.id.repository_et);
+        mUserAbout = (EditText) findViewById(R.id.about_et);
+
+        mUserInfo = new ArrayList<>();
+        mUserInfo.add(mUserPhone);
+        mUserInfo.add(mUserEmail);
+        mUserInfo.add(mUserVk);
+        mUserInfo.add(mUserGit);
+        mUserInfo.add(mUserAbout);
+
+        mFloatingActionButton.setOnClickListener(this);
 
         setupToolbar();
         setupDrawer();
 
         if (savedInstanceState == null) {
             showSnackbar("Запущено впервые");
+
         } else {
             showSnackbar("Запущено повторно");
+            mCurrentEditMode = savedInstanceState.getInt(ConstantManager.EDIT_MODE_KEY, 0);
+            changeViewMode(mCurrentEditMode);
         }
 
         Log.d(TAG, "onCreate()");
@@ -103,7 +138,8 @@ public class MainActivity extends BaseActivity {
     private void setupDrawer() {
         NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
 
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView
+                .OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
                 showSnackbar(item.getTitle().toString());
@@ -112,5 +148,61 @@ public class MainActivity extends BaseActivity {
                 return false;
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab:
+                if (mCurrentEditMode == 0) {
+                    changeViewMode(1);
+                } else {
+                    changeViewMode(0);
+                }
+
+                break;
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(ConstantManager.EDIT_MODE_KEY, mCurrentEditMode);
+    }
+
+
+    /**
+     * Переключает view, в режим редактирования и обратно
+     * @param mode - режим view, 1 - режим редактирования, 0 - режим просмотра
+     */
+    private void changeViewMode(int mode) {
+        if (mode == 1) {
+            for (EditText userValue : mUserInfo) {
+                userValue.setEnabled(true);
+                userValue.setFocusable(true);
+                userValue.setFocusableInTouchMode(true);
+            }
+
+            mFloatingActionButton.setImageResource(R.drawable.ic_done_24dp);
+        } else {
+            for (EditText userValue : mUserInfo) {
+                userValue.setEnabled(false);
+                userValue.setFocusable(false);
+                userValue.setFocusableInTouchMode(false);
+            }
+
+            mFloatingActionButton.setImageResource(R.drawable.ic_edit_24dp);
+        }
+
+        mCurrentEditMode = mode;
+    }
+
+    private void loadUserInfoValue() {
+
+    }
+
+    private void saveUserInfoValue() {
+
     }
 }
--- app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
+++ app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
@@ -4,5 +4,6 @@ package com.softdesign.devintensive.utils;
  * Created by OsIpoFF on 24.06.16.
  */
 public class ConstantManager {
-    public static String TAG_PREFIX = "DEV ";
+    public static final String TAG_PREFIX = "DEV ";
+    public static final String EDIT_MODE_KEY = "EDIT_MODE";
 }
