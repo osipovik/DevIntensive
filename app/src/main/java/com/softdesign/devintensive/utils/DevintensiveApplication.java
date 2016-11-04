047a33b584366554b66e23c7d06fdf86127f0a80
Ivan Osipov <osipovik89@gmail.com>
2016-11-04 18:59:14 +0300

    вынес данные о пользователе из прокручивающейся области, и добавил изменение её высоты при сворачивании collpsingToolbar

--- app/src/main/java/com/softdesign/devintensive/utils/DevintensiveApplication.java
+++ app/src/main/java/com/softdesign/devintensive/utils/DevintensiveApplication.java
@@ -1,6 +1,7 @@
 package com.softdesign.devintensive.utils;
 
 import android.app.Application;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
@@ -9,15 +10,25 @@ import android.preference.PreferenceManager;
  */
 public class DevintensiveApplication extends Application {
     public static SharedPreferences sSharedPreferences;
+    private static Context sContext;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         sSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sContext = getApplicationContext();
     }
 
     public static SharedPreferences getSharedPreferences() {
         return sSharedPreferences;
+    }
+
+    /**
+     * Return application context
+     * @return Context
+     */
+    public static Context getContext() {
+        return sContext;
     }
 }
