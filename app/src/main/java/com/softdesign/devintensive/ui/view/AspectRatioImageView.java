67ea79105be629c863e00d943ed9490e85c3f159
Ivan Osipov <osipovik89@gmail.com>
2016-11-06 03:42:23 +0300

    добавил новый экран со списком, и верстку для элементов списка + custom ImageView

---
+++ app/src/main/java/com/softdesign/devintensive/ui/view/AspectRatioImageView.java
@@ -0,0 +1,39 @@
+package com.softdesign.devintensive.ui.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.content.res.TypedArray;
+import android.widget.ImageView;
+
+import com.softdesign.devintensive.R;
+
+/**
+ * Created by OsIpoFF on 06.11.16.
+ */
+
+public class AspectRatioImageView extends ImageView {
+
+    private static final float DEFAULT_ASPECT_RATIO = 1.73f;
+
+    private final float mAspectRatio;
+
+    public AspectRatioImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.AspectRatioImageView);
+        mAspectRatio = a.getFloat(R.styleable.AspectRatioImageView_aspect_ratio, DEFAULT_ASPECT_RATIO);
+        a.recycle();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int newWidth;
+        int newHeight;
+
+        newWidth = getMeasuredWidth();
+        newHeight = (int) (newWidth / mAspectRatio);
+
+        setMeasuredDimension(newWidth, newHeight);
+    }
+}
